ee8f3508455d890c2bd04e65e8589841eb7c1937
Valerio Galante <email@gmail.com>
2021-12-17 12:51:12 +0100

    Creazione grafico torta JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,5 +37,37 @@ function onLoad() {
             }
         }
     });
-
+    const ctxvalerio = document.getElementById('chart-valerio').getContext('2d');
+    const chartValerio = new Chart(ctxvalerio, {
+        type: 'pie',
+        data: {
+            labels: ['IOS', 'Android', 'BlackBurry', 'Symbian', 'Others'],
+            datasets: [{
+                label: '# of Votes',
+                data: [30, 10, 20, 15, 30],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 }
