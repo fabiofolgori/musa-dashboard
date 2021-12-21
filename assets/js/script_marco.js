9c4ae75002e695e123d13fe80e303b726245be2e
Marco Insegno <marcoinse@gmail.com>
2021-12-21 14:02:26 +0100

    jQuery + slides

---
+++ assets/js/script_marco.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $('.main-carousel').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true,
+        autoPlay: true
+    });
+
+    console.log('ciao');
+
+});
